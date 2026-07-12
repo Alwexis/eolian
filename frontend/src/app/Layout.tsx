871b6d154983e5b2adc4d2695f1d410d231ae200
Ariel Silva <redthelegendarytrainer@gmail.com>
2026-07-12 11:26:08 -0400

    feat: sidebar & layout

--- frontend/src/app/Layout.tsx
+++ frontend/src/app/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../shared/components/Sidebar";
+import { Header } from "@/shared/components/Header";
 
 export function Layout() {
   return (
     <div className='antialiased font-sans bg-white min-h-dvh min-w-dvw grid grid-cols-[auto_1fr]'>
       <Sidebar />
       <div className='grid grid-rows-[auto_1fr]'>
-        <header className='h-14 items-center gap-x-3 border-b border-b-neutral-200 px-4 backdrop-blur-sm'></header>
+        <Header />
         <main className='p-4'>
           <Outlet />
         </main>
--- frontend/src/shared/components/LinkWrapper.tsx
+++ frontend/src/shared/components/LinkWrapper.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import { cn } from "../lib/utils/cn";
+import { cn } from "../lib/utils";
 
 interface Props {
   children: React.ReactNode;
